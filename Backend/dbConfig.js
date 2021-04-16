02cb01ab7b65497fd6abb487d237b3617688ea29
Tharun sekar <tharunsd23@gmail.com>
2021-04-16 17:55:15 +0530

    second commit

--- Backend/dbConfig.js
+++ Backend/dbConfig.js
@@ -5,9 +5,9 @@ const mysql = require("mysql");
 
 // DB connection Instance
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Password@123",
+  host: process.env.NODE_ENV === "production" ? "database-1.caufirjh3w4l.us-east-2.rds.amazonaws.com" : "localhost",
+  user:  process.env.NODE_ENV === "production" ? "admin": "root",
+  password:  process.env.NODE_ENV === "production" ? "tharun@23" : "Password@123",
   database: "ecommerce1",
   port: 3306,
   timeout: 60000,
